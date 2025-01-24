3a3eebd42a34cef15d84a12fef101990c90c604b
nick <nick@MacBookPro.lan>
2025-01-25 01:18:44 +0800

    Remove system commands from build script

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y python3-dev gcc
-
 # Install Python dependencies
 python -m pip install --upgrade pip
 python -m pip install wheel
