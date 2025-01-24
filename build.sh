0387703eac54e52e64d0474130d8124c62655cce
nick <nick@MacBookPro.lan>
2025-01-25 01:16:09 +0800

    Update numpy version and build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -o errexit
 
+# Install system dependencies
+apt-get update
+apt-get install -y python3-dev gcc
+
 # Install Python dependencies
 python -m pip install --upgrade pip
-python -m pip install gunicorn
-python -m pip install -r requirements.txt 
+python -m pip install wheel
+python -m pip install -r requirements.txt
+python -m pip install gunicorn 
