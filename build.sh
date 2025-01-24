d1671b355a4993314f1aa543429b6e368a44bf3b
nick <nick@MacBookPro.lan>
2025-01-25 01:22:08 +0800

    Use pre-built numpy wheel

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 # Install Python dependencies
 python -m pip install --upgrade pip
-python -m pip install wheel
+python -m pip install --only-binary :all: numpy==1.24.3
 python -m pip install -r requirements.txt
 python -m pip install gunicorn 
