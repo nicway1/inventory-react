9dbe7d73f3745f64d23a5c71bf82fec551003c10
nicway1 <nicway1@github.com>
2025-02-14 02:33:55 +0800

    Switch to Application Password for SMTP authentication

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ app.config.update(
     MAIL_PORT=int(os.environ.get('MAIL_PORT', '587')),
     MAIL_USE_TLS=True,
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME', 'support@truelog.site'),
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD', '123456'),
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD', 'chme-pwlx-ealo-rbwb'),  # Application Password
     MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER', 'support@truelog.site'),
     MAIL_DEBUG=True  # Enable debug mode
 )
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
 # Add your project directory to the sys.path
 path = '/home/nicway2/inventory'
@@ -8,15 +12,20 @@ if path not in sys.path:
 
 # Set Python path and enable debugging
 os.environ['PYTHONPATH'] = '/home/nicway2/inventory'
+os.environ['FLASK_DEBUG'] = '1'
 os.environ['MAIL_DEBUG'] = '1'
-os.environ['MAIL_USE_TLS'] = 'True'
 
-# Set environment variables
+# Email configuration - explicit settings
 os.environ['MAIL_SERVER'] = 'mail.privateemail.com'
 os.environ['MAIL_PORT'] = '587'
+os.environ['MAIL_USE_TLS'] = 'True'
+os.environ['MAIL_USE_SSL'] = 'False'
 os.environ['MAIL_USERNAME'] = 'support@truelog.site'
-os.environ['MAIL_PASSWORD'] = '123456'
+os.environ['MAIL_PASSWORD'] = 'chme-pwlx-ealo-rbwb'  # Application Password
 os.environ['MAIL_DEFAULT_SENDER'] = 'support@truelog.site'
+os.environ['MAIL_MAX_EMAILS'] = '1'  # Limit for testing
+os.environ['MAIL_SUPPRESS_SEND'] = 'False'
+os.environ['MAIL_ASCII_ATTACHMENTS'] = 'False'
 
 #db
 os.environ['SECRET_KEY'] = 'your-secret-key-here'
@@ -24,6 +33,9 @@ os.environ['DATABASE_URL'] = 'sqlite:////home/nicway2/inventory/inventory.db'
 
 # Import your Flask app
 from app import app as application
+
+# Enable debug logging for Flask-Mail
+application.logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
     application.run() 
