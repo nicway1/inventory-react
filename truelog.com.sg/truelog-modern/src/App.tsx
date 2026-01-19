8717c160f59ea9bf41ab4d5936247281fe388977
nicway1 <nicway2@gmail.com>
2026-01-19 15:57:15 +0800

    Add basename to Router for /site deployment

--- truelog.com.sg/truelog-modern/src/App.tsx
+++ truelog.com.sg/truelog-modern/src/App.tsx
@@ -19,7 +19,7 @@ import Compliance from './pages/services/Compliance';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <main>
